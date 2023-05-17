2d50719937fc4a3caa9967b5a5250f7a980014de
Rolf Jagerman <jagerman@google.com>
2023-05-17 11:46:30 -0700

    Bump version to v0.3.0
    PiperOrigin-RevId: 532857779

--- rax/__init__.py
+++ rax/__init__.py
@@ -41,7 +41,7 @@ from rax._src.t12n import bound_t12n
 from rax._src.t12n import gumbel_t12n
 from rax._src.t12n import segment_t12n
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "dcg2_lambdaweight",
